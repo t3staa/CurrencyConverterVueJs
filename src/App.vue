99fd9032080293aa68f465451093bb449977f473
Leonardo <testa.leo@outlook.com>
2024-07-30 18:04:41 -0300

    comecando estilização

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template> 
   <div id="app">
-    <h1>Conversão de Moedas</h1>
-    <ConversorComponetUSD_BRL moedaA="USD" moedaB="BRL"/>
+    <h1 class="titulo">Conversão de Moedas</h1>
+    <ConversorComponent moedaA="USD" moedaB="BRL"/>
   </div>
+  <footer>
+    <p>Autor: Leonardo Testa | Data: 30/07/2024</p>
+    <p><a href="mailto:testa.leo@outlook.com">testa.leo@outlook.com</a></p>
+  </footer>
 </template>
 
 <script> 
-import ConversorComponetUSD_BRL from "./components/Conversor.vue";
+import ConversorComponent from "./components/Conversor.vue";
 
 export default {
   name: 'App',
   components: {
-    ConversorComponetUSD_BRL
+    ConversorComponent
   }
 };
 </script>
@@ -23,6 +27,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 40px;
+  margin-top: 80px;
+  padding: 2px;
+}
+footer {
+  padding: 5px;
+  background-color:#86929f;
+  color: white;
+  box-shadow: 0 .5rem 1.5rem rgba(0,0,0,.1);
+  border-radius: .5rem;
+  border: .1rem solid rgba(0,0,0,.1);
+  margin-right: 75px;
+  margin-left: 75px;
 }
 </style>
--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -1,36 +1,68 @@
 <template>
     <div class="conversor">
         <h2>{{moedaA}} Para {{moedaB}}</h2>
-        <input type="text" v-model="moedaA_value" placeholder="Valor em USD:">
+        <input type="text" v-model="moedaA_value" placeholder="Valor em Dólar:">
         <input type="button" value="Converter" v-on:click="converterUSD_BRL">
-        <h2>{{moedaB_value}}</h2>
+        <h2>{{resultUSD_BRL}}</h2>
+    </div>
+
+    <div class="conversor">
+        <h2>{{moedaB}} Para {{moedaA}}</h2>
+        <input type="text" v-model="moedaB_value" placeholder="Valor em Real:">
+        <input type="button" value="Converter" v-on:click="converterBRL_USD">
+        <h2>{{resultBRL_USD}}</h2>
     </div>
 </template>
 
 <script>
 export default {
-    name: "ConversorComponetUSD_BRL",
+    name: "ConversorComponent",
     props: ["moedaA", "moedaB"],
     data(){
         return{
-            moedaA_value : "",
-            moedaB_value : 0
+            moedaA_value: "",
+            moedaB_value: "",
+            resultUSD_BRL: 0,
+            resultBRL_USD: 0
         }
     },
 
     methods:{
         converterUSD_BRL(){
-            this.moedaB_value = (this.moedaA_value * 5.6161006159).toFixed(2) + " R$";
-            return this.moedaB_value;
+            this.resultUSD_BRL = (this.moedaA_value * 5.6161006159).toFixed(2) + " Reais";
+            return this.resultUSD_BRL;
+        },
+        converterBRL_USD(){
+            this.resultBRL_USD = (this.moedaB_value * 0.178059488).toFixed(2) + " Dolares";
+            return this.resultBRL_USD;
+            
         }
     }
 };
 </script>
 
-<style scoped>
+<style>
+
 .conversor{
-   padding: 20px;
-   max-width: 300px; 
-   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    margin: 20px;
+    padding: 50px;
+    padding-left: 215px;
+    padding-right: 215px;
+    max-width: auto; 
+    box-shadow: 0 .5rem 1.5rem rgba(0,0,0,.1);
+    border-radius: .5rem;
+    border: .1rem solid rgba(0,0,0,.1);
+    display: inline-block;
+    justify-content: space-between;
+}
+
+.titulo{
+    padding: 10px;
+    margin-right: 35px;
+    margin-left: 35px;
+    max-width: auto;
+    box-shadow: 0 .5rem 1.5rem rgba(0,0,0,.1);
+    border-radius: .5rem;
+    border: .1rem solid rgba(0,0,0,.1);
 }
 </style>
